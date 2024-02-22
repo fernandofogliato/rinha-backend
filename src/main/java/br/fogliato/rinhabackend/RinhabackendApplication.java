5abd88a7240693f43d34e8b2c0dd1e90ff0427e1
Fernando Fogliato <fernandofogliato@gmail.com>
2024-02-21 21:12:13 -0300

    Initial commit

---
+++ src/main/java/br/fogliato/rinhabackend/RinhabackendApplication.java
@@ -0,0 +1,13 @@
+package br.fogliato.rinhabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fogliato/rinhabackend/RinhabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fogliato.rinhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
